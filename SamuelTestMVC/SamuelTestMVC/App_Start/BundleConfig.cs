57c56a4660aa500faa1b35d3ad792c4bedbd97f2
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2017-08-23 11:30:32 -0400

    Change BOOTSTRAP Template #1

--- SamuelTestMVC/SamuelTestMVC/App_Start/BundleConfig.cs
+++ SamuelTestMVC/SamuelTestMVC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SamuelTestMVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
