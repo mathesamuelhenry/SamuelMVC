0832b0bc079b89fb7d4c19983471600fca75e28b
Mathe, Samuel <samuel.mathe@lexisnexisrisk.com>
2017-08-23 11:14:37 -0400

    Initial version

---
+++ SamuelTestMVC/SamuelTestMVC/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using SamuelTestMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SamuelTestMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie newMovie = new Movie() { Name = "Shrek" };
+
+            return View(newMovie);
+        }
+
+
+    }
+}
---
+++ SamuelTestMVC/SamuelTestMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SamuelTestMVC.MvcApplication" Language="C#" %>
